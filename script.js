37928fcdca4622828a447b9e9a71994a9982ab08
Kurt Riecken <kurt.riecken@gmail.com>
2023-09-27 09:30:50 -0400

    button functionality updated; length validation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global variables
 const lettersUpper = ["A", "B", "C"];
 const lettersLower = ["a", "b", "c"];
+let passwordLen = 0;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -15,8 +16,13 @@ function writePassword() {
 }
 
 function generatePassword(array) {
-  let passwordLen = prompt(`Please choose a length between 8 and 128 characters.`)
-  let finalPassword = [];
+  let flag = validInput();
+
+  while(!flag) {
+    flag = validInput();
+  }
+ 
+  let finalPassword = ["d"];
   if (confirm(`Use upper case letter?`)) {
     finalPassword = finalPassword.concat(lettersUpper);
   }
@@ -27,6 +33,27 @@ function generatePassword(array) {
   console.log(finalPassword[0]);
   returnString = `Your password is ${finalPassword[0]}`;
   return returnString;
+}
+
+function validInput() {
+//get input
+passwordLen = prompt("Please provide a number between 8 and 128");
+console.log(`input is ${passwordLen}`);
+
+//if valid, great, return true
+if (isNaN(passwordLen)) {
+  alert("Input is not a number; please provide a valid number!");
+  return false;
+}
+else if (passwordLen < 8 || passwordLen > 128) {
+  alert("Number must be between 8 and 128, please.");
+  return false;
+}
+else {
+  alert("Thank you!");
+  return true;
+}
+return false;
 }
 
 // Add event listener to generate button
